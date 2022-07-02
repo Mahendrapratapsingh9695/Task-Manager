4adf7007b04df815dfc965d9d3beffba8857ac88
Mahendra Pratap Singh <73751516+Mahendrapratapsingh9695@users.noreply.github.com>
2022-07-02 21:30:07 +0530

    full commit

---
+++ frontend/src/Footer/Footer.js
@@ -0,0 +1,18 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="text-center text-lg-start">
+      <div className="mt-3">
+        <div className="row align-items-center">
+          <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
+            <p className="text-uppercase colorDesign footer-p">Task Manager</p>
+            <p> Built by Mahendra</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
